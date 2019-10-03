0dafbc21aae332106c51b082123340b117d02c79
Ashish Bende <ashishbende@cnollege.net>
2019-10-02 21:19:48 -0700

    initial commit

---
+++ catkin_ws/src/scripts/test_navigation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export TURTLEBOT_3D_SENSOR=kinect
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
---
+++ catkin_ws/src/scripts/test_slam.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export TURTLEBOT_3D_SENSOR=kinect
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
+sleep 5
+
+xterm -e "rosrun gmapping slam_gmapping" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
